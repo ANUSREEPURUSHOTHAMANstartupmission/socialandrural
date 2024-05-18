0befddede41c5a501d0a15c95d052271f273427b
ANUSREEPURUSHOTHAMANstartupmission <anusree@startupmission.in>
2024-05-18 11:16:52 +0530

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 
+import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ANUSREEPURUSHOTHAMANstartupmission.github.io',
+  base:'/socialandrural',
   integrations: [tailwind(), svelte()]
 });
+
+
+
