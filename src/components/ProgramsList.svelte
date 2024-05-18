e531d776f519c81857ae491109795160e65394b2
ANUSREEPURUSHOTHAMANstartupmission <anusree@startupmission.in>
2024-05-18 11:10:42 +0530

    changes

---
+++ src/components/ProgramsList.svelte
@@ -0,0 +1,36 @@
+<script>
+    import programs from '../data/eoi.json';
+
+    function navigateToDetailPage(id) {
+        window.location.href = `/${id}`;
+    }
+
+</script>
+
+<div class="px-6 md:px-8">
+    <div class="md:grid-cols-3 lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 grid gap-4 px-4">
+        {#each programs as program}
+            <div class="pb-8 relative flex flex-col cursor-pointer border shadow-2xl bg-white" on:click={() => navigateToDetailPage(program.id)}>
+                <div class="card cursor-pointer m-4" style="background-image: url('/img/{program.image1}');"></div>
+                <div class="pt-2">
+                    <div class="text-black w-full p-4 text-lg flex uppercase" style="font-family: Jost, sans-serif;">{program.title}</div>
+                </div>
+            </div>
+        {/each}
+    </div>
+</div>
+
+<style>
+    .card {
+        background-color: white;
+        height: 14rem;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+
+    .card-title:hover {
+        cursor: default;
+        color: rgb(249 66 30 / var(--tw-text-opacity));
+    }
+</style>
---
+++ src/pages/[id]/index.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { onMount } from 'svelte';
+    import { page } from '$app/stores';
+    import programs from '../../data/eoi.json';
+
+    let program;
+    let id;
+
+    // Get the id from the URL
+    $: id = $page.params.id;
+
+    // Find the program with the matching id
+    $: program = programs.find(p => p.id === id);
+</script>
+
+{#if program}
+    <div class="container mx-auto p-4">
+        <h1 class="text-2xl font-bold">{program.title}</h1>
+        <img src="/img/{program.image1}" alt="{program.title}" class="w-full object-contain h-64 mx-auto my-4">
+        <p class="text-justify">{program.desc}</p>
+        <a href="{program.apply}" class="text-blue-500 underline">Apply Now</a>
+    </div>
+{:else}
+    <p>Loading...</p>
+{/if}
